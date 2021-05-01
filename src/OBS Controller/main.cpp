f4c17b9aa85b1976ea500cc8f5ad86241ce26e67
unknown <newjincar33@gmail.com>
2021-05-01 16:19:23 +0900

    코드 정리

--- src/OBS Controller/main.cpp
+++ src/OBS Controller/main.cpp
@@ -33,62 +33,72 @@ int main(int argc, char* argv[]) {
 	}
 
 	switch (commandId) {
-	case commandIds::STREAMING_START: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_streaming_start");
-		RemoteFunction<decltype(obs_frontend_streaming_start)> streaming_start(process, procedure->procAddress);
-		streaming_start.Call();
+	case commandIds::STREAMING_START:
+		MakeRemoteFunction<decltype(obs_frontend_streaming_start)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_streaming_start"
+			).Call();
 		break;
-	}
-	case commandIds::STREAMING_STOP: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_streaming_stop");
-		RemoteFunction<decltype(obs_frontend_streaming_stop)> streaming_stop(process, procedure->procAddress);
-		streaming_stop.Call();
-		break;
-	}
 
-	case commandIds::RECORDING_START: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_recording_start");
-		RemoteFunction<decltype(obs_frontend_recording_start)> recording_start(process, procedure->procAddress);
-		recording_start.Call();
+	case commandIds::STREAMING_STOP:
+		MakeRemoteFunction<decltype(obs_frontend_streaming_stop)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_streaming_stop"
+			).Call();
 		break;
-	}
-	case commandIds::RECORDING_STOP: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_recording_stop");
-		RemoteFunction<decltype(obs_frontend_recording_stop)> recording_stop(process, procedure->procAddress);
-		recording_stop.Call();
-		break;
-	}
-	case commandIds::RECORDING_RESUME: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_recording_pause");
-		RemoteFunction<decltype(obs_frontend_recording_pause)> recording_pause(process, procedure->procAddress);
-		recording_pause.Call(false);
-		break;
-	}
-	case commandIds::RECORDING_PAUSE: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_recording_pause");
-		RemoteFunction<decltype(obs_frontend_recording_pause)> recording_pause(process, procedure->procAddress);
-		recording_pause.Call(true);
-		break;
-	}
 
-	case commandIds::REPLAY_BUFFER_START: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_replay_buffer_start");
-		RemoteFunction<decltype(obs_frontend_replay_buffer_start)> replay_buffer_start(process, procedure->procAddress);
-		replay_buffer_start.Call();
+	case commandIds::RECORDING_START:
+		MakeRemoteFunction<decltype(obs_frontend_recording_start)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_recording_start"
+			).Call();
 		break;
-	}
-	case commandIds::REPLAY_BUFFER_STOP: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_replay_buffer_stop");
-		RemoteFunction<decltype(obs_frontend_replay_buffer_stop)> replay_buffer_stop(process, procedure->procAddress);
-		replay_buffer_stop.Call();
+	case commandIds::RECORDING_STOP:
+		MakeRemoteFunction<decltype(obs_frontend_recording_stop)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_recording_stop"
+			).Call();
 		break;
-	}
-	case commandIds::REPLAY_BUFFER_SAVE: {
-		auto procedure = process.modules().GetExport(L"obs-frontend-api.dll", "obs_frontend_replay_buffer_save");
-		RemoteFunction<decltype(obs_frontend_replay_buffer_save)> replay_buffer_save(process, procedure->procAddress);
-		replay_buffer_save.Call();
+	case commandIds::RECORDING_RESUME:
+		MakeRemoteFunction<decltype(obs_frontend_recording_pause)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_recording_pause"
+			).Call(false);
 		break;
-	}
+	case commandIds::RECORDING_PAUSE:
+		MakeRemoteFunction<decltype(obs_frontend_recording_pause)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_recording_pause"
+			).Call(true);
+		break;
+
+	case commandIds::REPLAY_BUFFER_START:
+		MakeRemoteFunction<decltype(obs_frontend_replay_buffer_start)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_replay_buffer_start"
+			).Call();
+		break;
+	case commandIds::REPLAY_BUFFER_STOP:
+		MakeRemoteFunction<decltype(obs_frontend_replay_buffer_stop)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_replay_buffer_stop"
+			).Call();
+		break;
+	case commandIds::REPLAY_BUFFER_SAVE:
+		MakeRemoteFunction<decltype(obs_frontend_replay_buffer_save)>(
+			process,
+			L"obs-frontend-api.dll",
+			"obs_frontend_replay_buffer_save"
+			).Call();
+		break;
 	}
 
 	process.Detach();
