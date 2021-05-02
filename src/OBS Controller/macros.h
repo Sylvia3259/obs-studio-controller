a3ad9870e060ab485e83c1dc8073e2700e052de3
unknown <newjincar33@gmail.com>
2021-05-02 19:16:43 +0900

    매크로 추가 및 코드 정리

--- src/OBS Controller/macros.h
+++ src/OBS Controller/macros.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <tchar.h>
+#include <Windows.h>
 #include <BlackBone/Process/RPC/RemoteFunction.hpp>
 
-#define REMOTE_CALL(func, ...) {\
-	MakeRemoteFunction<decltype(func)>(\
+#define ERROR(message) {\
+	MessageBox(NULL, _T(message), _T("Error"), MB_ICONERROR);\
+}
+
+#define REMOTE_CALL(process, func, ...) {\
+	blackbone::MakeRemoteFunction<decltype(func)>(\
 		process, L"obs-frontend-api.dll", #func\
 	).Call(__VA_ARGS__);\
 }
--- src/OBS Controller/main.cpp
+++ src/OBS Controller/main.cpp
@@ -1,6 +1,4 @@
 #include <fstream>
-#include <tchar.h>
-#include <Windows.h>
 #include <BlackBone/Process/Process.h>
 #include <BlackBone/Misc/Utils.h>
 #include "constants.h"
@@ -12,7 +10,7 @@ int main(int argc, char* argv[]) {
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
 
 	if (argc != 2) {
-		MessageBox(NULL, _T("Invaild arguments"), _T("Error"), MB_ICONERROR);
+		ERROR("Invaild arguments");
 		return 1;
 	}
 
@@ -21,13 +19,12 @@ int main(int argc, char* argv[]) {
 	if (command != commands.end())
 		commandId = command->second;
 	else {
-		MessageBox(NULL, _T("Invaild arguments"), _T("Error"), MB_ICONERROR);
+		ERROR("Invaild arguments");
 		return 1;
 	}
 
 	wstring obsPath, obsDirectory, obsFilename;
-	auto currentPath = Utils::GetExeDirectory();
-	wifstream file(currentPath + L"\\obs-studio.txt");
+	wifstream file(Utils::GetExeDirectory() + L"\\obs-studio.txt");
 	if (file.is_open()) {
 		getline(file, obsPath);
 		obsDirectory = Utils::GetParent(obsPath);
@@ -35,70 +32,71 @@ int main(int argc, char* argv[]) {
 		file.close();
 	}
 	else {
-		MessageBox(NULL, _T("Failed to open the file"), _T("Error"), MB_ICONERROR);
+		ERROR("Failed to open the file");
 		return 1;
 	}
 
 	Process process;
+	NTSTATUS status;
 	if (commandId == commandIds::OBS_STUDIO_START) {
-		auto status = process.CreateAndAttach(obsPath, false, false, L"", obsDirectory.c_str());
+		status = process.CreateAndAttach(obsPath, false, false, L"", obsDirectory.c_str());
 		if (!NT_SUCCESS(status)) {
-			MessageBox(NULL, _T("Cannot create process"), _T("Error"), MB_ICONERROR);
+			ERROR("Cannot create process");
 			return 1;
 		}
 		process.Detach();
 		return 0;
 	}
 	else if (commandId == commandIds::OBS_STUDIO_STOP) {
-		auto status = process.Attach(obsFilename.c_str());
+		status = process.Attach(obsFilename.c_str());
 		if (!NT_SUCCESS(status)) {
-			MessageBox(NULL, _T("Cannot attach to process"), _T("Error"), MB_ICONERROR);
+			ERROR("Cannot attach to process");
 			return 1;
 		}
 		SetLastNtStatus(STATUS_SUCCESS);
 		status = process.Terminate();
 		if (!NT_SUCCESS(status)) {
-			MessageBox(NULL, _T("Failed to terminate process"), _T("Error"), MB_ICONERROR);
+			ERROR("Failed to terminate process");
 			return 1;
 		}
 		process.Detach();
 		return 0;
 	}
 
-	auto status = process.Attach(obsFilename.c_str());
+	status = process.Attach(obsFilename.c_str());
 	if (!NT_SUCCESS(status)) {
-		MessageBox(NULL, _T("Cannot attach to process"), _T("Error"), MB_ICONERROR);
+		ERROR("Cannot attach to process");
 		return 1;
 	}
 	switch (commandId) {
 	case commandIds::STREAMING_START:
-		REMOTE_CALL(obs_frontend_streaming_start);
+		REMOTE_CALL(process, obs_frontend_streaming_start);
 		break;
 	case commandIds::STREAMING_STOP:
-		REMOTE_CALL(obs_frontend_streaming_stop);
+		REMOTE_CALL(process, obs_frontend_streaming_stop);
 		break;
 
 	case commandIds::RECORDING_START:
-		REMOTE_CALL(obs_frontend_recording_start);
+		REMOTE_CALL(process, obs_frontend_recording_start);
 		break;
 	case commandIds::RECORDING_STOP:
-		REMOTE_CALL(obs_frontend_recording_stop);
+		REMOTE_CALL(process, obs_frontend_recording_stop);
 		break;
 	case commandIds::RECORDING_RESUME:
-		REMOTE_CALL(obs_frontend_recording_pause, false);
+		REMOTE_CALL(process, obs_frontend_recording_pause, false);
 		break;
 	case commandIds::RECORDING_PAUSE:
-		REMOTE_CALL(obs_frontend_recording_pause, true);
+		REMOTE_CALL(process, obs_frontend_recording_pause, true);
 		break;
 
 	case commandIds::REPLAY_BUFFER_START:
-		REMOTE_CALL(obs_frontend_replay_buffer_start);
+		REMOTE_CALL(process, obs_frontend_replay_buffer_start);
 		break;
 	case commandIds::REPLAY_BUFFER_STOP:
-		REMOTE_CALL(obs_frontend_replay_buffer_stop);
+		REMOTE_CALL(process, obs_frontend_replay_buffer_stop);
 		break;
 	case commandIds::REPLAY_BUFFER_SAVE:
-		REMOTE_CALL(obs_frontend_replay_buffer_save);
+		REMOTE_CALL(process, obs_frontend_replay_buffer_save);
 		break;
 	}
 	process.Detach();
